1c332b8119878c679adf1f710b255dde5bd190de
Cirenio-Lopez <cireniobrylopez@gmail.com>
2023-04-26 00:41:32 -0500

    users.js removed

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -33,56 +33,6 @@ export default function Dashboard() {
                   </div>
                 </div>
               </li>
-              <li>
-                <div className="friend-wrapper">
-                  <div className="img-wrapper">
-                    <img
-                      src="https://www.pngarts.com/files/10/Default-Profile-Picture-Transparent-Image.png"
-                      alt="friend"
-                    />
-                  </div>
-                  <div className="info">
-                    <div className="name">Kevin Hart</div>
-                    <div className="flex">
-                      <div className="borrow-owe">
-                        Borrowed: <span className="amount">$0</span>
-                      </div>
-                      <div className="borrow-owe">
-                        Owed: <span className="amount">$0</span>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="buttons-wrapper">
-                    <button className="button">Borrow</button>
-                    <button className="button">Provide</button>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div className="friend-wrapper">
-                  <div className="img-wrapper">
-                    <img
-                      src="https://www.pngarts.com/files/10/Default-Profile-Picture-Transparent-Image.png"
-                      alt="friend"
-                    />
-                  </div>
-                  <div className="info">
-                    <div className="name">Kevin Hart</div>
-                    <div className="flex">
-                      <div className="borrow-owe">
-                        Borrowed: <span className="amount">$0</span>
-                      </div>
-                      <div className="borrow-owe">
-                        Owed: <span className="amount">$0</span>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="buttons-wrapper">
-                    <button className="button">Borrow</button>
-                    <button className="button">Provide</button>
-                  </div>
-                </div>
-              </li>
             </ul>
           </div>
         </div>
